1686b1a2feb7578ce5e569ce33bb639647950559
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-06-01 13:57:05 +0300

    implement WebView wrapper

---
+++ WebView.js
@@ -0,0 +1,56 @@
+import EventEmitter from 'events';
+import React, { Component } from 'react';
+import { WebView as NativeWebView } from 'react-native';
+
+import registerRNMessageChannel from './dist/injected';
+
+export class WebView extends Component {
+  constructor(props) {
+    super(props);
+    this.messagesChannel = new EventEmitter();
+  }
+
+  render() {
+    return (
+      <NativeWebView
+        {...this.props}
+        onMessage={this.onMessage}
+        ref={this._refWebView}
+      />
+    );
+  }
+
+  _refWebView = (webview) => {
+    this.webview = webview;
+  }
+
+  onMessage = (event) => {
+    const { data } = event.nativeEvent;
+    const parsedMsg = JSON.parse(data);
+
+    switch (parsedMsg.type) {
+      case 'json':
+        this.messagesChannel.emit('json', JSON.parse(parsedMsg.payload));
+        break;
+      case 'text':
+        this.messagesChannel.emit('text', parsedMsg.payload);
+        break;
+    }
+  }
+
+  componentDidMount() {
+    this.webview.injectJavaScript(registerRNMessageChannel);
+  }
+
+  send(string) {
+    this.webview.injectJavaScript(`(function (global) {
+      global.RNMessageChannel.emit('text', ${string});
+    })(window)`);
+  }
+
+  sendJSON(json) {
+    this.webview.injectJavaScript(`(function (global) {
+      global.RNMessageChannel.emit('json', ${JSON.stringify(json)});
+    })(window)`);
+  }
+}
