47e846a12c90fe7710ecd65f13a8a5f9928ddc6a
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2018-02-21 16:33:24 +0100

    Fix red screen due to window.postMessage override

--- src/react-native/hoc.js
+++ src/react-native/hoc.js
@@ -7,9 +7,28 @@ import { Events } from '../shared/constants';
 export const withMessaging = (WebView) => {
   return class WebViewWithMessaging extends React.PureComponent {
     render() {
+      const patchPostMessageFunction = () => {
+        var originalPostMessage = window.postMessage;
+        var patchedPostMessage = function(message, targetOrigin, transfer) {
+          originalPostMessage(message, targetOrigin, transfer);
+        };
+
+        patchedPostMessage.toString = () => {
+          return String(Object.hasOwnProperty).replace(
+            'hasOwnProperty',
+            'postMessage'
+          );
+        };
+        window.postMessage = patchedPostMessage;
+      };
+
+      const patchPostMessageJsCode =
+        '(' + String(patchPostMessageFunction) + ')();';
       return (
         <WebView
           {...this.props}
+          injectedJavaScript={patchPostMessageJsCode}
+          javaScriptEnabled={true}
           onMessage={this.handleWebViewMessage}
           ref={this.refWebView}
         />
